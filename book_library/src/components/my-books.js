62297235650d8d326fce9c2a6c7a3f51569f08c6
Tomatomac <seanwilliams712@gmail.com>
2020-12-01 13:21:25 +0000

    Added frontend
    Added frontend

---
+++ book_library/src/components/my-books.js
@@ -0,0 +1,11 @@
+import React, {Component } from 'react';
+
+export default class MyBooks extends Component {
+    render(){
+        return (
+            <div>
+            <p> This should show you all the books you have checked out and give you the option to return them</p>
+            </div>
+        )
+    }
+}
---
+++ book_library/src/components/view-books.js
@@ -0,0 +1,12 @@
+import React, {Component } from 'react';
+
+export default class ViewBooks extends Component {
+    render(){
+        return (
+            <div>
+            <p> This should show the list of all the books</p>
+            <p>The user should also be able to filter or search here</p>
+            </div>
+        )
+    }
+}
